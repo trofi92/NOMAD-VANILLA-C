77f7e9b58251537a84e82f884754b168be31c1c3
trofi92 <84131398+trofi92@users.noreply.github.com>
2022-08-11 16:56:38 +0900

    initial commit

---
+++ DAY11/src/app.js
@@ -0,0 +1,35 @@
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34"
+];
+
+
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const newArray = [...colors.keys()];
+
+button.addEventListener("click", function () {
+  const getIndexA = colors[Math.floor(Math.random() * newArray.length)];
+  const getIndexB = colors[Math.floor(Math.random() * newArray.length)];  
+
+  if (getIndexA != getIndexB)
+  { body.style.background = `linear-gradient(${getIndexA}, ${getIndexB})` }
+  else{body.style.background = "linear-gradient(#e66465, #9198e5)" }
+
+})
